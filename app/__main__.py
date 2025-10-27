151ac165b96c3bcdb5335b6455ec2d7a282622d4
Noah Lamont <noah.lamont@epitech.eu>
2025-10-27 12:35:47 +0100

    .

--- app/__main__.py
+++ app/__main__.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
-app = Flask(__name__)
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b"<h1>Hello from Whanos Kubernetes!</h1>")
 
-@app.route('/')
-def helloIndex():
-    return 'Hello world!'
-
-app.run(host='0.0.0.0', port=8080)
+HTTPServer(('0.0.0.0', 8080), Handler).serve_forever()
